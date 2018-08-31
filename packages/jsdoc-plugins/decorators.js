def3539feef29060c33f74f04d031fb0bcedc2bb
Anselm McClain <atm@xh.io>
2018-08-31 10:45:45 -0700

    WIP on comment updates to better support generated JSDocs

--- packages/jsdoc-plugins/decorators.js
+++ packages/jsdoc-plugins/decorators.js
@@ -1,3 +1,6 @@
+const classesByFile = {};
+const ptRanges = {};
+
 exports.handlers = {
 
     beforeParse: function(e) {
@@ -5,6 +8,7 @@ exports.handlers = {
             [/@HoistModel\(\)\n/gm, ''],
             [/@HoistService\(\)\n/gm, ''],
             [/@HoistComponent\(\)\n/gm, ''],
+            [/@HotkeysTarget\n/gm, ''],
             [/@LayoutSupport\n/gm, ''],
             [/@computed\n/gm, ''],
             [/@observable\n/gm, ''],
@@ -14,11 +18,58 @@ exports.handlers = {
             [/^ *@action\n/gm, '']
         ];
 
+        // Strip decorators entirely (for now).
         subs.forEach(it => {
             e.source = e.source.replace(it[0], it[1]);
         });
+    },
 
-        // console.log(e.source);
+    symbolFound: function(e) {
+        const f = e.filename;
+
+        // Associate file with Class
+        // TODO - problematic if we declare multiple classes within a file - needs to be more targeted.
+        if (e.code.node.type == 'ClassDeclaration') {
+            if (classesByFile[f]) {
+                console.warn(`File already associated with class: ${f} | ${classesByFile[f]}`);
+            } else {
+                classesByFile[f] = e.code.node.id.name;
+                // console.log(`${f} -> ${e.code.node.id.name}`);
+            }
+        }
+
+        // Bookmark range of any propType declarations
+        if (e.code.name == 'propTypes') {
+            ptRanges[e.filename] = {
+                start: e.range[0],
+                end: e.range[1]
+            };
+        }
+    },
+
+    // For each doclet that is to be documented, determine if the symbol falls within a known
+    // proptypes range for the class and if so force proptypes to appear as instance members.
+    newDoclet: function(e) {
+        const {doclet} = e,
+            {meta} = doclet,
+            filename = `${meta.path}/${meta.filename}`,
+            fileClass = classesByFile[filename],
+            ptRange = ptRanges[filename];
+
+        if (fileClass && ptRange && !doclet.undocumented) {
+            const dlStart = doclet.meta.range[0],
+                inPropTypes = dlStart >= ptRange.start && dlStart <= ptRange.end;
+
+            if (inPropTypes) {
+                doclet.scope = 'instance';
+                doclet.longname = `${fileClass}#${doclet.name}`;
+                doclet.memberof = fileClass;
+            }
+        }
+    },
+
+    parseComplete: function() {
+        // console.log(ptRanges);
     }
 
 };
